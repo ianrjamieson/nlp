7a175a264021545bd1425d94da7ba3938b946c2a
Ian Jamieson <ianj@iastate.edu>
2018-09-01 09:23:54 -0500

    added some changes

--- getdata.py
+++ getdata.py
@@ -1,10 +1,13 @@
 import sqlite3
 
-conn = sqlite3.connect('reddit.db')
+conn = sqlite3.connect('database.sqlite')
 
 cursor = conn.execute("SELECT body FROM May2015 WHERE score > 5000")
 output = ""
 for row in cursor:
-    output = output + cursor
+    output = output + row[17]
+
+with open('result.text', 'w') as file:
+    file.write(output)
     
 
